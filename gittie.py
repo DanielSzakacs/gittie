396f963891daeb9571a44d100f376e60402b4a5b
Daniel <daniel.soakch@gmail.com>
2018-02-21 09:38:18 +0100

    ok

--- gittie.py
+++ gittie.py
@@ -1,12 +1,6 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
-<<<<<<< HEAD
-def joke():
-    print("Can a kangaroo jump higher than a house?" + "\n" + "Of course. The house doesn't jump at all.")
-=======
 def add(a,b):
     print(a+b)
 
-add(2,3)
->>>>>>> 0a260ec1868241f1104c35d6eb0c920f7988b848
